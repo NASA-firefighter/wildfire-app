8ed515b2ea24123ac88e3337e67d151f4a850970
Dayeon Yoo <udayeon@naver.com>
2024-10-06 00:20:36 -0600

    ✨ Add endangered animals page (#11)

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,6 @@
 // src/Header.tsx
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
@@ -11,13 +11,18 @@ const Header: React.FC = () => {
             <Link to="/">Overview</Link>
           </li>
           <li style={navItemStyle}>
-            <Link to="/how-wildfire-affects">How Wildfire Affects Our System</Link>
+            <Link to="/how-wildfire-affects">
+              How Wildfire Affects Our System
+            </Link>
           </li>
           <li style={navItemStyle}>
             <Link to="/wildfire-map">Wildfire Map</Link>
           </li>
           <li style={navItemStyle}>
             <Link to="/firefighter-game">Firefighter Game</Link>
+          </li>
+          <li style={navItemStyle}>
+            <Link to="/endangered-animals">Endangered Animals</Link>
           </li>
         </ul>
       </nav>
@@ -26,20 +31,20 @@ const Header: React.FC = () => {
 };
 
 const headerStyle: React.CSSProperties = {
-  backgroundColor: '#282c34',
-  padding: '10px',
-  color: 'white',
+  backgroundColor: "#282c34",
+  padding: "10px",
+  color: "white",
 };
 
 const navListStyle: React.CSSProperties = {
-  listStyleType: 'none',
+  listStyleType: "none",
   padding: 0,
-  display: 'flex',
-  justifyContent: 'space-around',
+  display: "flex",
+  justifyContent: "space-around",
 };
 
 const navItemStyle: React.CSSProperties = {
-  margin: '0 15px',
+  margin: "0 15px",
 };
 
 export default Header;
